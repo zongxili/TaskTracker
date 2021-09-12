bbd15642357ecd0cfd7add8123e1b353578e499d
zongxili <zongxiliwork@gmail.com>
2021-09-12 13:38:06 -0700

    added JSON mocked server

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,11 @@ const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+      <Button
+        color={showAdd ? 'red' : 'green'}
+        text={showAdd ? 'Close' : 'Add'}
+        onClick={onAdd}
+      />
     </header>
   )
 }
