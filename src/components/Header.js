c2212e0d161b4d590f0c706fce2c529ba5d56dc1
zongxili <zongxiliwork@gmail.com>
2021-09-12 12:03:12 -0700

    added CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({ title }) => {
   return (
     <div>
       <h1>{title}</h1>
-    </div>
+    </div >
   )
 }
 
@@ -16,5 +16,11 @@ Header.defaultProps = {
 Header.propTypes = {
   title: propTypes.string.isRequired,
 };
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black'
+// };
 
 export default Header;
