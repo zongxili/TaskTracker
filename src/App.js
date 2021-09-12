74a3e84faa25b4c84c13c9ff0929f79837c8e64c
zongxili <zongxiliwork@gmail.com>
2021-09-12 13:19:49 -0700

    added more functions to addTask function

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ function App() { // root component
     }
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -45,7 +52,7 @@ function App() { // root component
   return (
     <div className="container">
       <Header title='Task Tracker' />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0
         ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
         : ('No Tasks to Show')
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,13 +1,27 @@
 import { useState } from 'react';
 
-const AddTask = () => {
+const AddTask = ({ onAdd }) => {
 
   const [text, setText] = useState('');
   const [day, setDay] = useState('');
   const [reminder, setReminder] = useState(false);
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!text) {
+      alert('Please add a task')
+      return;
+    }
+
+    onAdd({ text, day, reminder });
+
+    setText('');
+    setDay('');
+    setReminder(false);
+  }
+
   return (
-    <form className='add-form'>
+    <form className='add-form' onSubmit={onSubmit}>
       <div className='form-contorl'>
         <label>Task</label>
         <input
@@ -32,6 +46,7 @@ const AddTask = () => {
         <label>Set Reminder</label>
         <input
           type="checkbox"
+          checked={reminder}
           value={reminder}
           onChange={(e) => setReminder(e.currentTarget.checked)}
         />
