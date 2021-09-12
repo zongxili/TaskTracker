5a9bfb79da0fc61a412f4fc871270e90b6002f2c
zongxili <zongxiliwork@gmail.com>
2021-09-12 14:20:10 -0700

    added PUT function

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() { // root component
     getTasks();
   }, []);
 
-
   // Fetch Tasks (Place it outside of useEffect so we can use it somewhere)
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks');
@@ -25,24 +24,54 @@ function App() { // root component
     return data;
   };
 
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
-  }
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(task),
+    });
+    const data = await res.json();
+    setTasks([...tasks, data]);
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = { id, ...task };
+    // setTasks([...tasks, newTask]);
+  };
 
   // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`,
+      { method: 'DELETE' }
+    );
+
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => { // Note
+    const taskToToggle = await fetchTask(id);
+
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(updTask),
+    });
+
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
         task.id === id
-          ? { ...task, reminder: !task.reminder }
+          ? { ...task, reminder: data.reminder }
           : task
       ));
   };
